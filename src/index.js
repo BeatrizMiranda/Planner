9f284c304c443f54ec114d62182b57c2cdace8b4
beatriz.miranda <beatriz.miranda@acct.global>
2020-01-31 21:22:28 -0300

    load after auth

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose } from 'redux'
 import RootReducer from './Store/Reducers/RootReducer';
-import { Provider } from 'react-redux'
+import { Provider, useSelector } from 'react-redux'
 import thunk from 'redux-thunk'
 import { reduxFirestore, getFirestore } from 'redux-firestore'
 import { getFirebase } from 'react-redux-firebase'
-import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
+import { ReactReduxFirebaseProvider, isLoaded } from 'react-redux-firebase'
 import { createFirestoreInstance } from 'redux-firestore'
 import FbConfig from './Config/FbConfig'
 
@@ -42,7 +42,16 @@ const rrfProps = {
     createFirestoreInstance
 };
 
-ReactDOM.render(<Provider store={store}><ReactReduxFirebaseProvider {...rrfProps}><App /></ReactReduxFirebaseProvider></Provider>, document.getElementById('root'));
+
+function AuthIsLoaded({ children }) {
+  const auth = useSelector(state => state.firebase.auth)
+  if (!isLoaded(auth)) return <div></div>;
+      return children
+}
+
+ReactDOM.render(<Provider store={store}><ReactReduxFirebaseProvider {...rrfProps}>
+   <AuthIsLoaded><App /> </AuthIsLoaded></ReactReduxFirebaseProvider>
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
