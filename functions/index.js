aa81c4efe35009eb5e4fb49d777e1f9cedfa0bf6
Beatriz Miranda <beatriz.miranda@acct.global>
2020-02-04 17:36:13 -0300

    changes

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+
+admin.initializeApp(functions.config().firebase);
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+
+exports.projectCreated = functions.firestore
+       .document('Project/{projectId}')
+       .onCreate(doc => {
+
+            const project = doc.data();
+            const notification = {
+                content: 'Added a new project',
+                user: `${project.authorFirstName} ${project.authorLastName}`
+            }
+})
